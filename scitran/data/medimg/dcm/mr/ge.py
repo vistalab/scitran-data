0ebebcb2f91c9292eb7249425dcaedce50f23804
Hua Wu <hua.wu02@gmail.com>
2019-07-24 15:20:36 -0700

    Do not try multicoil conversion for DWI data

--- scitran/data/medimg/dcm/mr/ge.py
+++ scitran/data/medimg/dcm/mr/ge.py
@@ -169,8 +169,8 @@ def parse_all(self):
 
     # TODO separatation of concern; identification vs dicom grouping
     recon_mode_flag = np.unique([self.getelem(d, TAG_RECON_FLAG, int, 0) for d in self._dcm_list])
-    if self.psd_type not in ['fieldmap']:
-        log.debug('recon_mode: 1, might be multicoil')
+    if not self.is_dwi and self.psd_type not in ['fieldmap']:
+        log.debug('might be multicoil')
         vol_counter = 0
         ref_position = self.getelem(self._hdr, 'ImagePositionPatient')
         for d in self._dcm_list:
